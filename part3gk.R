9a6bc9647effa6a5bda26a3d31a79becbdc38c63
gavimk <gavriella_keyles@ucsb.edu>
2019-10-21 13:56:56 -0700

    add infant mortality graph

--- part3gk.R
+++ part3gk.R
@@ -11,3 +11,9 @@ library(here)
 db <- read_csv(here::here("data", "disease_burden.csv")) %>% 
   clean_names() %>% 
   rename(deaths_per_100k = death_rate_per_100_000)
+
+# Create a subset and graph
+
+db_sub <- db %>% 
+  filter(country_name %in% c("Afghanistan", "Somalia", "United States", "Japan"))
+         
